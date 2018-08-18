d267ec7f0384dcefdf184ab030d233ef16d11aa2
omezquita <omezquita@users.noreply.github.com>
2018-08-18 17:19:54 -0400

    Update TrimCleanJoinRange.vb

--- TrimCleanJoinRange.vb
+++ TrimCleanJoinRange.vb
@@ -7,6 +7,7 @@ Dim cl As Range
 Dim S As String
 
 S = ""
+  'To use the VBA version of Trim remove the period in front of .Trim
 For Each cl In Rng.Cells
         With Application.WorksheetFunction
             S = S & prefix & .Trim(.Clean(cl.Value)) & suffix & Sep
